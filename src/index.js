8d0bb1b7ebae8628fc2a6c432f1b2f28a0f14740
olegkizz <olegkizz1998@gmail.com>
2018-03-01 01:51:26 +0300

    Created Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+	var count2 = 0, count5 = 0;
+	for(var i = 1;i <= number; ++i)
+	 {	
+	 	var k = i;
+	 	while(k % 2 == 0)
+	 		{
+	 			++count2;
+	 			k /= 2;
+	 		}
+	 	while(k % 5 == 0)
+	 		{
+	 			++count5;
+	 			k /= 5;
+	 		}
+	 }
+	 	
+ return Math.min(count2,count5)
+ }
